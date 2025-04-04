2ec40f89cb4eae5852badd4118bbd508dd97d425
GiovanniP9 <pisaniellogiovanni53@gmail.com>
2025-04-04 16:16:12 +0200

    Update negozio.py

--- negozio.py
+++ negozio.py
@@ -1,4 +1,26 @@
+import logging
 
+
+def is_called(func):
+    
+    def wrapper(*args, **kwargs):
+        logging.info(f"{func.__name__} is called")
+        res = func(*args, **kwargs)
+        logging.info(f"{func.__name__}is executed with args:{args} and kwargs: {kwargs}")
+        return res
+    return wrapper
+
+
+class Donut:
+    """Donut represents a real donut you can buy in a store"""
+    def __init__(self, name: str, sale_price: float, production_cost: float) -> float:
+        self.name = name
+        self.price = sale_price
+        self.prod_cost = production_cost
+        
+    @is_called    
+    def calculate_profit(self):
+        return self.price - self.prod_cost
 
 #CREAZIONE DELLA CLASSE FABBRICA
 class store:
@@ -21,7 +43,7 @@ class store:
             self.inventory[product_name] -= quantity # Aggiorna la quantità in inventario
             profit = product.calculate_profit() * quantity # Calcola il profitto
             self.total_profit += profit # Aggiungi il profitto totale
-            print(f"sold {quantity} of {product.name}. Profit: {profit}.") 
+            print(f"sold {quantity} of {product}. Profit: {profit}.") 
         else:
             print("Product not available or insufficient quantity.") 
     
